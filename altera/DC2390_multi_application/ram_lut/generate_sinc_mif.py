398806330928f27c798d69871b42ab9bd618c770
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2015-09-19 14:50:15 +0000

    Adding option to window data with Hann window.
    git-svn-id: svn://subzero.engineering.linear.com:23691/FPGA_sketchbook/trunk@4021 2d5ccaef-af5a-4037-8d26-727f02bf293a

--- altera/DC2390_multi_application/ram_lut/generate_sinc_mif.py
+++ altera/DC2390_multi_application/ram_lut/generate_sinc_mif.py
@@ -5,19 +5,25 @@ Created on Wed Jul 15 10:35:05 2015
 @author: mark_t
 """
 
-
+import numpy as np
 from matplotlib import pyplot as plt
 from math import sin
 
+windowme = True
+
 #Generate funky SINC data
 total_samples = 65536 # n.BuffSize
-data = total_samples * [0] 
+data = total_samples * [0]
+window = np.hanning(len(data))
 for i in range(0, total_samples):
     x = ((i - 32768) / (512.0)) + 0.0001 # Add a tiny offset to avoid divide by zero
     data[i] = int(32000 * (sin(x) / x))
+    if windowme == True:
+        data[i] *= window[i]
+    data[i] = int(data[i])
     if(data[i] < 0):
         data[i] += 65536
-    
+
 plt.figure(1)
 plt.plot(data)
 plt.show()
