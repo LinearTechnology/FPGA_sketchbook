0c0db3679849004a296aa08f2fc14a5fff888521
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-06-06 17:35:50 +0000

    Adding 32-bit up counter for memory testing
    git-svn-id: svn://subzero.engineering.linear.com:23691/FPGA_sketchbook/trunk@5225 2d5ccaef-af5a-4037-8d26-727f02bf293a

---
+++ altera/DC2390_multi_application/upcount_32/upcount_32_inst.v
@@ -0,0 +1,9 @@
+upcount_32	upcount_32_inst (
+	.clock ( clock_sig ),
+	.cnt_en ( cnt_en_sig ),
+	.data ( data_sig ),
+	.sclr ( sclr_sig ),
+	.sload ( sload_sig ),
+	.cout ( cout_sig ),
+	.q ( q_sig )
+	);
