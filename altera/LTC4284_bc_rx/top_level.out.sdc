f7b87dd07bd018f6bc55dbb5bd457ad69ea46335
mthoren <mthoren@2d5ccaef-af5a-4037-8d26-727f02bf293a>
2016-07-14 23:15:54 +0000

    Adding LTC4284_bc_rx project!
    git-svn-id: svn://subzero.engineering.linear.com:23691/FPGA_sketchbook/trunk@5422 2d5ccaef-af5a-4037-8d26-727f02bf293a

---
+++ altera/LTC4284_bc_rx/top_level.out.sdc
@@ -0,0 +1,109 @@
+## Generated SDC file "top_level.out.sdc"
+
+## Copyright (C) 1991-2014 Altera Corporation. All rights reserved.
+## Your use of Altera Corporation's design tools, logic functions 
+## and other software and tools, and its AMPP partner logic 
+## functions, and any output files from any of the foregoing 
+## (including device programming or simulation files), and any 
+## associated documentation or information are expressly subject 
+## to the terms and conditions of the Altera Program License 
+## Subscription Agreement, the Altera Quartus II License Agreement,
+## the Altera MegaCore Function License Agreement, or other 
+## applicable license agreement, including, without limitation, 
+## that your use is for the sole purpose of programming logic 
+## devices manufactured by Altera and sold by Altera or its 
+## authorized distributors.  Please refer to the applicable 
+## agreement for further details.
+
+
+## VENDOR  "Altera"
+## PROGRAM "Quartus II"
+## VERSION "Version 14.1.0 Build 186 12/03/2014 SJ Full Version"
+
+## DATE    "Wed Dec 09 09:19:39 2015"
+
+##
+## DEVICE  "EP4CE22F17C6"
+##
+
+
+#**************************************************************
+# Time Information
+#**************************************************************
+
+set_time_format -unit ns -decimal_places 3
+
+
+
+#**************************************************************
+# Create Clock
+#**************************************************************
+
+create_clock -name {systwm_clk} -period 20.000 -waveform { 0.000 10.000 } [get_ports {sys_clk}]
+create_clock -name {SPI_clk} -period 250.000 -waveform { 0.000 125.000 } [get_ports {spi_sck}]
+
+
+#**************************************************************
+# Create Generated Clock
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Clock Latency
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Clock Uncertainty
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Input Delay
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Output Delay
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Clock Groups
+#**************************************************************
+
+
+
+#**************************************************************
+# Set False Path
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Multicycle Path
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Maximum Delay
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Minimum Delay
+#**************************************************************
+
+
+
+#**************************************************************
+# Set Input Transition
+#**************************************************************
+
---
+++ altera/LTC4284_bc_rx/top_level.v
@@ -0,0 +1,119 @@
+`timescale 1ns / 10ps   // Each unit time is 1ns and the time precision is 10ps
+
+/*
+    Created by: Noe Quintero
+    E-mail: nquintero@linear.com
+
+    Copyright (c) 2015, Linear Technology Corp.(LTC)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those
+    of the authors and should not be interpreted as representing official policies,
+    either expressed or implied, of Linear Technology Corp.
+
+
+    Description:
+        The purpose of this module is to interface with the DC2459A demo board as 
+        a digital signal generator.
+*/
+
+module top_level
+(
+    // System signals
+    sys_clk,
+    reset_btn,
+    trig,
+
+    // SPI interface
+    spi_ss,
+    spi_sck,
+    spi_miso,
+    spi_mosi,
+
+    // LTC1668 interface
+    ltc1668_data
+);
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Port declaration
+    ///////////////////////////////////////////////////////////////////////////
+
+    // System signals
+    input               sys_clk;
+    input               reset_btn;
+    output              trig;
+
+    // SPI interface
+    input               spi_ss;
+    input               spi_sck;
+    output              spi_miso;
+    input               spi_mosi;
+
+    // LTC1668 interface
+    output  [15:0]  ltc1668_data;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Module parameters
+    ///////////////////////////////////////////////////////////////////////////
+
+    localparam NCO_DEFAULT_TUNING_WORD = 32'd858993; // 10 KHz
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Internal signals
+    ///////////////////////////////////////////////////////////////////////////
+
+    reg         [31:0]  shift_reg;
+    wire                reset_n;
+    wire        [17:0]  nco_sin;
+    reg         [31:0]  dff;
+
+    //*************************************************************************
+
+    // Connect the reset to the reset button
+    assign reset_n = reset_btn;
+
+    //*************************************************************************
+
+    assign ltc1668_data = 16'b0;
+    assign trig =  1'b0;
+
+	 LTC4284_bc_rx LTC4284_bc_rx_inst(
+	 
+	 
+
+           .clk_24m (sys_clk),       // Clock
+           .enb(1'b1),           // High to enable packet detect, low to abort
+                                   // (enb low doesn't alter the SPI read buffer)
+           .sdao(reset_btn),          // Serial output from 4284
+           .cs_n(spi_ss),          // SPI chip select#
+           .sck(spi_sck),           // SPI clock, in mode 0 or 3.
+                                   // The falling edge of cs_n will cause the first
+                                   // bit to be output.  Subsequent changes happend on
+                                   // sck falling edges.  However, a rising edge must
+                                   // happen first, before a falling edge is used
+          .miso(spi_miso)           // Data output
+
+	 );
+
+
+endmodule
